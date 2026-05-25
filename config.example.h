5399a230999db31e40fa5f297ddb50c0559a0793
Divyansh Sachdev <89719842+divyansh-sachdev@users.noreply.github.com>
2026-05-25 11:00:00 +0530

    Initial commit: Dual DC motor PWM driver and VL53L0X ToF range finder

---
+++ config.example.h
@@ -0,0 +1,14 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+#define MOTOR_L_PWM 25
+#define MOTOR_L_DIR 26
+#define MOTOR_R_PWM 27
+#define MOTOR_R_DIR 14
+
+#define US_TRIG_PIN 18
+#define US_ECHO_PIN 19
+
+#define SAFE_DISTANCE_CM 20
+
+#endif
